5533c73dbaaf47887e9c9ee3c800ce768b90de87
Big_Bread <38143572+staierb@users.noreply.github.com>
2025-01-23 09:05:37 -0500

    Add files via upload

---
+++ Groups within a group.ps1
@@ -0,0 +1,4 @@
+﻿$Group = "MDOT-CADD-Users"
+$user = "William.T.Rohman"
+$path = "C:\Users\$user\Documents\Groups.txt"
+Get-ADGroupMember $Group|?{$_.objectClass -eq "Group"}|Select -Expand Name|Out-File $path 
